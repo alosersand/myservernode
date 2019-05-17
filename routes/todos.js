d42a94ee8959aaa6ab9c6d726de85bd30135c958
lazyBit99 <alex.marchi99@gmail.com>
2019-05-17 11:46:12 +0200

    step 4d, 4e

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,6 @@ const lista = require('../data/lista')
 router.get('/', (req, res) => {
     let todos = lista.todos
     res.json(todos)
-
 })
 
 router.get('/:id', (req, res) => {
@@ -15,5 +14,55 @@ router.get('/:id', (req, res) => {
     res.json(todos.filter((todo) => todo.id === id))
 })
 
+router.post('/form', (req, res) => {
+    const body = req.body
+    const {
+        id,
+        titolo,
+        descrizione
+    } = req.body
+
+    status = {}
+
+    if (titolo) {
+        status.code = 'success'
+        status.message = `Aggiunto '${titolo}' alla lista`
+    } else {
+        status.code = 'error'
+        status.message = 'titolo assente'
+        status.campo = 'titolo'
+    }
+
+    res.send(status)
+})
+
+router.put('/form', (req, res) => {
+    status = {}
+
+    status.code = 'success'
+    status.message = `Database aggiornato`
+
+    res.send(status)
+})
+
+router.delete('/form', (req, res) => {
+    const body = req.body
+    const {
+        id
+    } = req.body
+
+    status = {}
+
+    if (id) {
+        status.code = 'success'
+        status.message = `Eliminato il ${id}° todo dalla lista`
+    } else {
+        status.code = 'error'
+        status.message = 'id assente'
+        status.campo = 'titolo'
+    }
+    
+    res.send(status)
+})
 
 module.exports = router
